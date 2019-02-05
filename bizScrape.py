883c92f273b4c3f31e1745e12e177efaaf3e2f2c
Felipe-ZC <fzunigacar@gmaiil.com>
2019-02-04 22:07:16 -0800

    Can retrieve HTML from search.sunbiz.org's search result page, needs WORK'

---
+++ bizScrape.py
@@ -0,0 +1,40 @@
+import re
+import mechanize
+import requests
+
+'''
+For search.sunbiz.org
+
+Mechanize Link obj:
+    absolute_url – The absolutized link URL 
+    url – The link URL 
+    base_url – The base URL against which this link is resolved 
+    text – The link text
+    tag – The link tag name 
+    attrs – The tag attributes
+
+TODO: Match regex from user input to the link object's url field. 
+
+TODO: Make a request to search.sunbiz.org with user input, validate that the
+user's input is a registered business entity in Florida.
+
+TODO: Validate that company input is a registered business entity in Florida
+and is active.
+'''
+
+#browser = mechanize.Browser()
+# browser.open('http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder=SPECTRUMETRX&searchTerm=SpecTruMetRx%2C%20Inc')
+#browser.open('http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder=SPECTRUM&searchTerm=spectrum')
+#links = browser.links()
+
+#print('Response: ', links)
+
+# for link in links:
+    # print ('Link text field: ', link.text)
+
+# Using requests
+searchQuery = input('Search sunbiz.org: ')
+link = f'http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder={searchQuery.upper()}&searchTerm={searchQuery}'
+html = requests.get(link).text
+
+print('HTML: ', html)
