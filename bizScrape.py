764825b1b4b4a55480aaa6898c69e16239f24fc8
Felipe-ZC <fzunigacar@gmaiil.com>
2019-02-05 22:57:13 -0800

    Returns list of tuples, each tuple contains the name of the active business searched for

--- bizScrape.py
+++ bizScrape.py
@@ -4,30 +4,13 @@ import requests
 '''
 For search.sunbiz.org
 
-TODO: Match regex from user input to the link object's url field. 
-
-TODO: Prep user input to match search.sunbiz.org's format. 
-
-TODO: Make a request to search.sunbiz.org with user input, validate that the
-user's input is a registered business entity in Florida.
-
-TODO: Validate that company input is a registered business entity in Florida
-and is active. 
-
 TODO: Input validation!!!!
 
 TODO: CLI (Command line interface)
+
+TODO: Encapsulate code into module or whatever python uses
+to package and prepare for depolyment.
 '''
-
-# Using requests
-searchQuery = input('Search sunbiz.org: ')
-formattedInput = searchQuery.upper()
-#print(formattedInput)
-
-# f strings are a new construct introduced in python 3.6, allows for string interpolation like in Node and other languages.
-link = f'http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder={searchQuery.upper()}&searchTerm={searchQuery}'
-html = requests.get(link).text
-#print('html: ', html)
 
 '''
 Use a regex to get all tr tags on the page (html).
@@ -37,9 +20,14 @@ Using two flags:
     - S makes the dot character (.) match any character including newlines.
 '''
 
-#tableRows = re.findall('\<tr\>.*\/tr\>', html, re.S)
-tableRows = re.findall('\<td.*\>', html)
-print(f'Found {len(tableRows)/3} results for {searchQuery}')
+# Using requests
+searchQuery = input('Search sunbiz.org: ')
+
+# f strings are a new construct introduced in python 3.6, allows for string interpolation like in Node and other languages.
+link = f'http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder={searchQuery.upper()}&searchTerm={searchQuery}'
+html = requests.get(link).text
+
+tableRows = re.findall('\<td.*', html)
 
 '''
 Look for a match in each <td> element, if a match is found on index i
@@ -49,17 +37,21 @@ index i + 2.
 This is a pretty basic approach to the problem but most of this is hacked
 af, needs review!
 '''
-print('searching for: ', searchQuery)
-matches = [] # Holds all matches found, with their index...
+parsed = [] # Holds all matches found, with their index...
 
 for index, row in enumerate(tableRows):
     if(re.search(searchQuery, row, re.I)):
-        print('Found a match for: ', searchQuery)
         # TODO: Check for out of bounds you dick!
         isActive = re.search('Active', tableRows[index + 2]);
-        matches.append((row, isActive))
+        companyName = re.search('\>[a-zA-Z0-9., ]+\<', row)
+        # Collect active businesses
+        if(companyName and isActive):
+            formattedName = re.sub('<|>', '', companyName.group(0))
+            parsed.append((formattedName, isActive.group(0)))
 
-print(f'Matches found for {searchQuery}: {matches}')
+
+print(f'Found {len(parsed)} active businesses registered under the name {searchQuery}')
+print(parsed)
 
 '''
 Return a list of results, let user
