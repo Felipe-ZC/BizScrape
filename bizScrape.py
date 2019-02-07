ef8320f9b6b88f1f373a87cef5f5894bf925e3dc
Felipe-ZC <fzunigacar@gmaiil.com>
2019-02-06 23:37:53 -0800

    Return data in JSON format

--- bizScrape.py
+++ bizScrape.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import json
 
 '''
 For search.sunbiz.org
@@ -8,14 +9,14 @@ TODO: Input validation!!!!
 
 TODO: CLI (Command line interface)
 
-TODO: Encapsulate code into module or whatever python uses
-to package and prepare for depolyment.
+TODO: Encapsulate code into module or whatever python uses to package and
+prepare for depolyment.
 '''
 
 '''
-Use a regex to get all tr tags on the page (html).
-Most of the anchor tags on the html page are the results to searchQuery, with some exceptions.
-Using two flags:
+Use a regex to get all td tags on the page (html).  Most of the anchor tags
+on the html page are the results to searchQuery, with some exceptions.  Using
+two flags:
     - M (multiline)
     - S makes the dot character (.) match any character including newlines.
 '''
@@ -27,13 +28,13 @@ def searchSunBiz(searchQuery):
     # f strings are a new construct introduced in python 3.6, allows for string interpolation like in Node and other languages.
     link = f'http://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults?inquiryType=EntityName&searchNameOrder={searchQuery.upper()}&searchTerm={searchQuery}'
     html = requests.get(link).text
-    tableRows = re.findall('\<td.*', html) 
+    tableRows = re.findall('\<td.*', html) #TODO: Find a better way to parse HTML lol
     parsed = [] # Holds all matches found, with their index...
 
     '''
-    Look for a match in each <td> element, if a match is found on index i
-    then the <td> element that describes the business' status is found on 
-    index i + 2. 
+    Look for a match in each <td> element, if a match is found on index i then
+    the <td> element that describes the business' status is found on index i +
+    2. 
 
     This is a pretty basic approach to the problem but most of this is hacked
     af, needs review!
@@ -43,7 +44,7 @@ def searchSunBiz(searchQuery):
              # TODO: Check for out of bounds you dick!
             isActive = re.search('Active', tableRows[index + 2]);
             companyName = re.search('\>[a-zA-Z0-9., ]+\<', row)
-            # Collect active businesses
+            # Save only active businesses
             if(companyName and isActive):
                 formattedName = re.sub('<|>', '', companyName.group(0))
                 parsed.append(formattedName)
@@ -57,12 +58,19 @@ choose from list in frontend.
 #TODO: Return resulting list as JSON!!!!! (Come on bro...)
 def main():
     searchQuery = input('Search sunbiz.org: ')
-    result = searchSunBiz(searchQuery)
+    results = searchSunBiz(searchQuery)
+    
+    # Output results
+    print(f'Found {len(results)} active businesses while searching for {searchQuery}')
+    print("--------- Results ---------")
 
-    print(f'Found {len(result)} active businesses registered while searching for {searchQuery}')
-    print(result)
+    for result in results:
+        print(result)
+    
+    # Return results as JSON
+    return json.dumps({'data' : results}, sort_keys=True,
+                        indent=None, separators=(',', ': '))
 
-    return result;
-
+# Main module    
 if __name__ == '__main__':
     main()
